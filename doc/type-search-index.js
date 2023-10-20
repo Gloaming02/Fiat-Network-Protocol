08722eed0d2865c7fc1ffb49126a3e13f5c0ad90
Zhengyan Hu <91642985+Gloaming02@users.noreply.github.com>
2023-10-19 18:31:11 -0700

    Add files via upload

---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"p":"foop.serialization","l":"ACK"},{"p":"foop.serialization","l":"Addition"},{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"foop.serialization","l":"Error"},{"p":"foop.serialization","l":"Message"},{"p":"foop.serialization","l":"MessageFactory"},{"p":"foop.serialization","l":"Register"}];updateSearchResults();
